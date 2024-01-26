d8ea0e6c91dbc2fb5f2b5da481635c00227536ae
baltazarparra <baltazarparra@outlook.com>
2024-01-26 14:52:44 -0300

    feat: loading

--- app/page.js
+++ app/page.js
@@ -1,9 +1,20 @@
+import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Hello</h1>
+      <h1 className="title">Novo projeto em desenvolvimento...</h1>
+      <Image
+        src="/logo.png"
+        width={250}
+        height={250}
+        alt="Logo"
+      />
+      <small>
+        R. Natale Petri, 580 - Jardim Samambaia, Barra Bonita - SP, 17342-500
+        <br /><b>(14) 99152-7687</b>
+      </small>
     </main>
   );
 }
